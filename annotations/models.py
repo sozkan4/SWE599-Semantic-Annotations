e5e5d593970010e664d8a4107782b0068511144c
sozkan4 <ozkansem@ahotmail.com>
2023-04-30 00:51:19 +0300

    fix the structure

---
+++ annotations/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+        
+class Annotation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Assumes you're using Django's built-in User model for authentication
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField()
+    annotation_type = models.CharField(max_length=255)  # E.g., 'text', 'image', etc.
+    target = models.TextField()  # Reference to the annotated text or image
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title}"
+
