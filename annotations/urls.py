fb3ed8de154d9e58ff25858ad12c462836624277
sozkan4 <ozkansem@ahotmail.com>
2023-05-09 02:20:15 +0300

    fix template structure

--- annotations/urls.py
+++ annotations/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 app_name = 'annotations'
 
 urlpatterns = [
-    path('', views.MovieListView.as_view(), name='movie_list'),
-    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='movie_detail'),
-    path('movies/<int:pk>/annotate/', views.AnnotationCreateView.as_view(), name='annotation_create'),
+    path('', views.MovieListView.as_view(), name='movies'),
+    path('movies/<int:movie_id>/', views.MovieDetailView.as_view(), name='annotations'),
+    path('movies/<int:movie_id>/annotate/', views.AnnotationCreateView.as_view(), name='add_annotation'),
+    path('accounts/login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
+
+
 ]
--- annotations/views.py
+++ annotations/views.py
@@ -1,33 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.decorators import method_decorator
-from django.views import View
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Movie, Annotation
 from .forms import AnnotationForm
 
-class MovieListView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies.html', {'movies': movies})
+class MovieListView(ListView):
+    model = Movie
+    template_name = 'annotations/movies.html'
 
-class MovieDetailView(View):
-    def get(self, request, movie_id):
-        movie = get_object_or_404(Movie, id=movie_id)
-        annotations = Annotation.objects.filter(movie=movie)
-        form = AnnotationForm()
-        return render(request, 'annotations.html', {'movie': movie, 'annotations': annotations, 'form': form})
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'annotations/movie_detail.html'
 
 @method_decorator(login_required, name='dispatch')
-class AnnotationCreateView(View):
-    def post(self, request, movie_id):
-        form = AnnotationForm(request.POST)
-        if form.is_valid():
-            annotation = form.save(commit=False)
-            annotation.user = request.user
-            annotation.movie_id = movie_id
-            annotation.save()
-            return redirect('annotations', movie_id=movie_id)
-        else:
-            movie = get_object_or_404(Movie, id=movie_id)
-            annotations = Annotation.objects.filter(movie=movie)
-            return render(request, 'annotations.html', {'movie': movie, 'annotations': annotations, 'form': form})
+class AnnotationCreateView(CreateView):
+    model = Annotation
+    form_class = AnnotationForm
+    template_name = 'annotations/annotation_create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.movie_id = self.kwargs['movie_id']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('annotations:movie_detail', args=[str(self.kwargs['movie_id'])])
