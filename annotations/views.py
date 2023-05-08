db53fa3f8208cf43c22ee0e0689a76e5b8c75b01
sozkan4 <ozkansem@ahotmail.com>
2023-05-09 01:54:28 +0300

    fix template structure

--- annotations/views.py
+++ annotations/views.py
@@ -1,22 +1,33 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, DetailView, CreateView
+from django.views import View
 from .models import Movie, Annotation
+from .forms import AnnotationForm
 
-class MovieListView(ListView):
-    model = Movie
-    template_name = 'annotations/movie_list.html'
+class MovieListView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies.html', {'movies': movies})
 
-class MovieDetailView(DetailView):
-    model = Movie
-    template_name = 'annotations/movie_detail.html'
+class MovieDetailView(View):
+    def get(self, request, movie_id):
+        movie = get_object_or_404(Movie, id=movie_id)
+        annotations = Annotation.objects.filter(movie=movie)
+        form = AnnotationForm()
+        return render(request, 'annotations.html', {'movie': movie, 'annotations': annotations, 'form': form})
 
 @method_decorator(login_required, name='dispatch')
-class AnnotationCreateView(CreateView):
-    model = Annotation
-    fields = ['content', 'annotation_type', 'target']
-    template_name = 'annotations/annotation_create.html'
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        form.instance.movie_id = self.kwargs['pk']
+class AnnotationCreateView(View):
+    def post(self, request, movie_id):
+        form = AnnotationForm(request.POST)
+        if form.is_valid():
+            annotation = form.save(commit=False)
+            annotation.user = request.user
+            annotation.movie_id = movie_id
+            annotation.save()
+            return redirect('annotations', movie_id=movie_id)
+        else:
+            movie = get_object_or_404(Movie, id=movie_id)
+            annotations = Annotation.objects.filter(movie=movie)
+            return render(request, 'annotations.html', {'movie': movie, 'annotations': annotations, 'form': form})
