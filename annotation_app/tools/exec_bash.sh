c2d816f06de91651be3002f3885cb92ba99d83c3
sozkan4 <ozkansem@ahotmail.com>
2023-05-19 15:23:36 +0300

    change docker config

--- annotation_app/tools/exec_bash.sh
+++
@@ -1,2 +0,0 @@
-
-CONTAINER_UID=$(id -u) CONTAINER_GID=$(id -g) docker-compose -f docker-compose.yml exec annotation_app bash
--- annotation_project/tools/exec_bash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-CONTAINER_UID=$(id -u) CONTAINER_GID=$(id -g) docker-compose -f docker-compose.yml exec app bash
