b233987cf83d253bd9547cfb8d2b8386e9a541e3
sozkan4 <ozkansem@ahotmail.com>
2023-05-19 15:47:09 +0300

    change admin for annotation

--- annotation_server/annotations/admin.py
+++ annotation_server/annotations/admin.py
@@ -5,6 +5,10 @@ from .models import Annotation
 
 @admin.register(Annotation)
 class AnnotationAdmin(admin.ModelAdmin):
-    list_display = ("id", "type")
-    search_fields = ("id", "type")
-    readonly_fields = ("id", "type", "body", "target")
+    list_display = ("annotation_id", "type_display")
+    search_fields = ("annotation_id", "annotation_type")
+    readonly_fields = ("annotation_id", "annotation_type", "body", "target", "context", "creation_datetime")
+
+    def type_display(self, obj):
+        return obj.annotation_type
+    type_display.short_description = 'Type'
