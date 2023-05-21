cd2d2f1c89a77b022b254c0b3cbded84c192a140
sozkan4 <ozkansem@ahotmail.com>
2023-05-22 02:35:50 +0300

    add annotation_create

--- annotation_server/annotations/urls.py
+++ annotation_server/annotations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 
-from .views import AnnotationDetail, AnnotationList, AnnotationSearch
+from .views import AnnotationDetail, AnnotationList, AnnotationSearch, AnnotationCreate
 
 urlpatterns = [
     path("", AnnotationList.as_view(), name="annotations"),
     path("search/", AnnotationSearch.as_view(), name="annotation_search"),
+    path("create/", AnnotationCreate.as_view(), name="annotation_create"),
     re_path(r"^(?P<annotation_id>[\w:/.#?!\-]+)/$", AnnotationDetail.as_view(), name="annotation_detail"),
 ]
